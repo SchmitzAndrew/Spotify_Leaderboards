da5e65ad31f189f034c2990fd08e390ab2fafcdf
A_SCHMITZ <schmitzandrew03@gmail.com>
2021-07-09 00:05:33 -0700

    scrapes soup

--- chartScraper.py
+++ chartScraper.py
@@ -3,3 +3,14 @@ import requests
 from bs4 import BeautifulSoup
 
 HOME_URL = "https://spotifycharts.com/regional/global/daily/latest"
+HEADERS = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
+
+response = requests.get(HOME_URL, headers=HEADERS)
+
+if response.status_code == 200:
+    print('Success!')
+else:
+    print('Fail :(')
+
+soup = BeautifulSoup(response.content.decode('utf-8'), "html.parser")
+print(soup.prettify())
